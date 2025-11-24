815076c7016b8ab54832b39ab50325ea4ec93007
Jan Linsen <janlinsench@gmail.com>
2025-11-24 06:02:22 +0100

    Added Amadeus API

--- src/amadeus.py
+++ src/amadeus.py
@@ -1,4 +1,24 @@
-# We use this for the Amadeus Travel API
+#We use this for the Amadeus Travel API
+
+from amadeus import Client, ResponseError
+from src.config import get_secret
+
+def get_amadeus_client():
+    client_id = get_secret("AMADEUS_CLIENT_ID")
+    client_secret = get_secret("AMADEUS_CLIENT_SECRET")
+
+    return Client(
+        client_id=client_id,
+        client_secret=client_secret,
+        hostname="test"
+    )
+
+ 
 
 def test_amadeus():
-    return "Amadeus Test successful"
+    amadeus = get_amadeus_client()
+    try:
+        response = amadeus.reference_data.locations.get(keyword="Zurich", subType="AIRPORT")
+        return response.data
+    except ResponseError as error:
+        return str(error)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from src.amadeus_client import test_amadeus
 
 st.set_page_config(
     page_title = "Travel Matching",
@@ -12,14 +13,17 @@ input1 = st.text_input("Test input")
 if input1:
     st.success(f"{input1} successful")
 
-from src.amadeus import test_amadeus
 from src.data import test_data
 from src.matching import test_matching
 from src.machinelearning import test_ml
 from src.visuals import test_visuals
 
-st.write("Amadeus", test_amadeus())
 st.write("Data", test_data())
 st.write("Matching", test_matching())
 st.write("Maching Learning", test_ml())
 st.write("Visuals", test_visuals())
+
+st.subheader("API Test: Amadeus")
+if st.button("Test connect"):
+    data = test_amadeus()
+    st.write(data)
