fd53cf1275eca0d25c20d3736a116fe4ee48caf1
Jan Linsen <janlinsench@gmail.com>
2025-11-24 04:36:29 +0100

    Added test code

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,15 @@ st.write("Welcome to our travel matching application")
 input1 = st.text_input("Test input")
 if input1:
     st.success(f"{input1} successful")
+
+from src.amadeus import test_amadeus
+from src.data import test_data
+from src.matching import test_matching
+from src.machinelearning import test_ml
+from src.visuals import test_visuals
+
+st.write("Amadeus"; test_amadeus())
+st.write("Data"; test_data())
+st.write("Matching"; test_matching())
+st.write("Maching Learning"; test_ml())
+st.write("Visuals"; test_visuals())
