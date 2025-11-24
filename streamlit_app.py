ad3c2368e51161d928013d8e0af1fc2e80600f33
Jan Linsen <janlinsench@gmail.com>
2025-11-24 08:25:45 +0100

    Added Open-Meteo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.amadeus import test_amadeus
+from src.weather import get_current_weather
 
 st.set_page_config(
     page_title = "Travel Matching",
@@ -27,3 +28,20 @@ st.subheader("API Test: Amadeus")
 if st.button("Test connect"):
     data = test_amadeus()
     st.write(data)
+
+st.subheader("API Test: Open-Meteo")
+if st.button("Weather for Barcelona"):
+    weather = get_current_weather()
+
+    if "error" in weather:
+        st.error("Error")
+    else:
+        st.write("Current weather in Barcelona")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric("Temperature", "{weather['temperature']} °C")
+            st.metric("Perceived", "{weather['apparent_temperature']} °C")
+        with col2:
+            st.metric("Humidity", "{weather['humidity']} %")
+            st.metric("Wind Speed", "{weather['wind_speed]} km/h")
+        st.caption("Data from {weather['time']}")
